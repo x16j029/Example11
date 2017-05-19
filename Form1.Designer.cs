5972e3bae01885af19bcb54df2c15f9f66df74e4
WIN_NT\x16j029 <x16j029@chiba-fjb.ac.jp>
2017-05-19 11:39:31 +0900

    if文

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.tbxYear = new System.Windows.Forms.TextBox();
+            this.bunJudge = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "西暦";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "年";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(96, 143);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 12);
+            this.lblResult.TabIndex = 2;
+            // 
+            // tbxYear
+            // 
+            this.tbxYear.Location = new System.Drawing.Point(48, 75);
+            this.tbxYear.Name = "tbxYear";
+            this.tbxYear.Size = new System.Drawing.Size(100, 19);
+            this.tbxYear.TabIndex = 3;
+            // 
+            // bunJudge
+            // 
+            this.bunJudge.Location = new System.Drawing.Point(177, 71);
+            this.bunJudge.Name = "bunJudge";
+            this.bunJudge.Size = new System.Drawing.Size(75, 23);
+            this.bunJudge.TabIndex = 4;
+            this.bunJudge.Text = "判定";
+            this.bunJudge.UseVisualStyleBackColor = true;
+            this.bunJudge.Click += new System.EventHandler(this.bunJudge_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.bunJudge);
+            this.Controls.Add(this.tbxYear);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "うるう年判定";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox tbxYear;
+        private System.Windows.Forms.Button bunJudge;
     }
 }
 
